0408e97be605389449df57f4123008509be420e4
Vladyslav Kovega <(8viper4@gmail.com)>
2017-04-21 17:43:27 +0300

    Добавл конструктор и сеттеры,геттеры для класса модели

--- src/sample/DocFileData.java
+++ src/sample/DocFileData.java
@@ -1,7 +1,12 @@
 package sample;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 /**
  * Created by VLAD on 05.04.2017.
@@ -10,14 +15,28 @@ public class DocFileData {
 
     private StringProperty fileName;
     private StringProperty textInFile;
+    private ObjectProperty<ArrayList<Text>> textArryInFile;
 
     public DocFileData(){
-        this(null,null);
+        this.fileName = null;
+        this.textInFile = null;
+        this.textArryInFile = null;
     }
 
     public DocFileData(String fileName,String textInFile) {
         this.fileName = new SimpleStringProperty (fileName);
         this.textInFile = new SimpleStringProperty (textInFile);
+
+    }
+
+    /**
+     * Конструктор обьекта по списку обьектов {@link javafx.scene.text.Text}
+     * @param fileName Название файла
+     * @param textList список обьектов {@link javafx.scene.text.Text}
+     */
+    public DocFileData(String fileName, ArrayList<Text> textList) {
+        this.fileName = new SimpleStringProperty(fileName);
+        this.textArryInFile = new SimpleObjectProperty<>(textList);
     }
 
     public String getFileName() {
@@ -42,5 +61,17 @@ public class DocFileData {
 
     public void setTextInFile(String textInFile) {
         this.textInFile.set(textInFile);
+    }
+
+    public ArrayList<Text> getTextArryInFile() {
+        return textArryInFile.get();
+    }
+
+    public ObjectProperty<ArrayList<Text>> textArryInFileProperty() {
+        return textArryInFile;
+    }
+
+    public void setTextArryInFile(ArrayList<Text> textArryInFile) {
+        this.textArryInFile.set(textArryInFile);
     }
 }
